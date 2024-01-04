1077a6fb660b43e3d7293c5bfc288984a32474dc
barty-s <117319197+barty-s@users.noreply.github.com>
2024-01-04 18:36:03 +0100

    Add content to templates and wire up urls

--- codestar/settings.py
+++ codestar/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_STORAGE = 'cloudinary_storage.StaticHashedCloudinaryStorage'
-STATIC_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_PORT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
